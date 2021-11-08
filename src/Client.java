29db8646249b256860cafec43ae10492dd31d3e8
unknown <alinserbanmusic@gmail.com>
2021-11-08 21:10:42 +0200

    added client class

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,29 @@
-public class Client extends Thread{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.util.concurrent.TimeoutException;
+
+public class Client {
+    public volatile boolean isRunning = true;
+    private Thread thread;
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void closeThread() {
+        isRunning = false;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Client with name " + this.getName() + " started succesfully!");
+        while (isRunning) {};
+        System.out.println("Client with name " + this.getName() + " stopped succesfully!");
+
+    }
 }
