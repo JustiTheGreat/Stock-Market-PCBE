d7335e7fc74bd3b6ec9098e840ea3edafda1162f
Justi <justinian.zaharia@gmail.com>
2021-11-09 18:16:33 +0200

    basic RabbitMQ comunnication (with all files added)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
-    public static void main(String[] args) {
-        Thread dispatcher = new Dispatcher();
-        dispatcher.start();
+    public static void main(String[] args) throws IOException, TimeoutException {
+//        Thread dispatcher = new Dispatcher();
+//        dispatcher.start();
+        Stock stock = new Stock("asdas",1,"asdasdas",1,1);
+        Subscriber subscriber = new Subscriber("name");
+        Publisher publisher = new Publisher("name");
+        System.out.println(stock.toString());
+        subscriber.subscribe();
+        publisher.publish(stock);
     }
 }
--- src/main/java/MyConnection.java
+++ src/main/java/MyConnection.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MyConnection {
+import java.io.*;
+
+public interface MyConnection {
+    static byte[] objectToByteArray(Object obj) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(obj);
+        return out.toByteArray();
+    }
+
+    static Object ByteArrayToObject(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        ObjectInputStream is = new ObjectInputStream(in);
+        return is.readObject();
+    }
 }
